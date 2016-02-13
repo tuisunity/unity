a31692a0e1498356218d932f7d5422d3c0036467
fencer06 <create.thegame0622@gmail.com>
2016-02-13 19:02:40 +0900

    雨

---
+++ Rain/Assets/Scripts/MyButton.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MyButton : MonoBehaviour
+{
+
+    public void OnClick()
+    {
+        Debug.Log("Button click!");
+        // 非表示にする
+        //gameObject.SetActive(false);
+
+        MyCanvas.SetActive("LightRain", true);
+
+    }
+}
